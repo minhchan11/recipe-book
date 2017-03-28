813ceee1fafe2313f48ae9159f924a5d24c71129
Lawrence Eby and Minh Phuong <lawrence+minh@>
2017-03-28 10:15:09 -0700

    add form, remove ingredientForm property of object

--- app/recipe.ts
+++ app/recipe.ts
@@ -1,4 +1,3 @@
 export class Recipe {
-  public ingredientForm: boolean = false;
   constructor(public name: string, public description: string, public steps: string, public ingredients: string[]) { }
 }
