96a9e935601eacd556082ad93ebe0a15f5376a24
Lawrence Eby and Minh Phuong <lawrence+minh@>
2017-03-28 08:49:06 -0700

    add template in app.component.ts, add AppComponent model, add object and constructor

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `<h1>Hello World</h1>`
+    template: `<div class="container">
+      <div class="row">
+        <div class="col-md-3" *ngFor = "let recipe of recipes">
+          <h4>{{recipe.name}}</h4>
+          <h5>{{recipe.description}}</h5>
+        </div>
+      </div>
+    </div>`
 })
 
 export class AppComponent{
-  
+  recipes: Recipe[] = [
+    new Recipe("cake","add flour"),
+    new Recipe("cake","add flour"),
+    new Recipe("cake","add flour"),
+  ];
+}
+
+export class Recipe {
+  constructor(public name: string, public description: string) { }
 }
